41558da9669c4ee419a76bb680d51557e3efe8ab
Ansbintariq <94162078+Ansbintariq@users.noreply.github.com>
2023-03-10 19:13:24 +0500

    local storage with clean code

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:shared_prefrnce/screens/get_started.dart';
 import 'package:shared_prefrnce/screens/loginpage.dart';
 
 import 'controller/login_controller.dart';
 
-void main() {
+void main() async {
   Get.put(LoginController());
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
@@ -19,10 +21,55 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
         primarySwatch: Colors.blue,
       ),
-      home: Started(),
+      home: Counter(),
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  @override
+  void initState() {
+    super.initState();
+    if (dataCount.read("key") != null) {
+      _count = dataCount.read("key");
+    }
+  }
+
+  int _count = 0;
+  final dataCount = GetStorage();
+  @override
+  Widget build(BuildContext context) {
+    dataCount.writeIfNull("key", 0);
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Text(
+                "${dataCount.read("key")}",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _count++;
+                      dataCount.write("key", _count);
+                    });
+                  },
+                  icon: Icon(Icons.add))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
